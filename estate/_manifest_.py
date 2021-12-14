f1d31b7c74f2efbd90cac3835c6a4b2ef10b0879
iSoft Solutions Ghana <ben.anyanmensah@gmail.com>
2021-12-14 23:25:17 +0000

    _estate

---
+++ estate/_manifest_.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name':'estate',
+    'version':'1.0',
+    'summary':'Real Estate Application',
+    'description':"",
+    'website':'http://www.isoftsols.com',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Property Listing"
