94cbfb3acdb2bac141722633f93be8633b12499b
iSoft Solutions Ghana <ben.anyanmensah@gmail.com>
2021-12-14 23:46:19 +0000

    Update __manifest__.py

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,21 +2,13 @@
 
 {
     'name': 'estate',
-    'version': '1.2',
-    'category': 'Sales/Estate',
-    'sequence': 15,
     'summary': 'Track Real Estate Sales',
     'description': "",
     'website': 'https://www.isoftsols.com',
     'depends': [
-        'base_setup',
+        'base',
     ],
-    'data': [
 
-    ],
-    'demo': [
-
-    ],
     'installable': True,
     'application': True,
     'auto_install': False
